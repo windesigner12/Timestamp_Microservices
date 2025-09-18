69d157ff1d4ae3f278d648fa8f58f0f65085625e
unknown <windesigner12>
2025-09-18 15:47:38 -0400

    fourth commit

--- server.js
+++ server.js
@@ -1,49 +1,33 @@
-// server.js
-const express = require("express");
-const app = express();
-// import cors from 'cors';
+// index.js
+// where your node app starts
 
-// var cors = require('cors');
-// app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+// init project
+var express = require('express');
+var app = express();
 
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC 
+var cors = require('cors');
+app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
-// Root endpoint
-app.get("/", (req, res) => {
-  res.send("Timestamp Microservice is running 🚀");
-});
-
+// http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
-// Without date param -> return current time
-app.get("/api", (req, res) => {
-  let date = new Date();
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-// With date param
-app.get("/api/:date", (req, res) => {
-  let dateParam = req.params.date;
 
-  // Check if it's a number (unix timestamp)
-  let date =
-    /^\d+$/.test(dateParam) 
-      ? new Date(parseInt(dateParam)) 
-      : new Date(dateParam);
-
-  // Handle invalid dates
-  if (date.toString() === "Invalid Date") {
-    return res.json({ error: "Invalid Date" });
-  }
-
-  res.json({
-    unix: date.getTime(),
-    utc: date.toUTCString(),
-  });
+// your first API endpoint... 
+app.get("/api/hello", function (req, res) {
+  res.json({greeting: 'hello API'});
 });
 
-// Listen on provided port
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-}
-);
+
+
+// Listen on port set in environment variable or default to 3000
+var listener = app.listen(process.env.PORT || 3000, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
+module.exports = app; // for testing
